c3c2d0bacb2b9524968ae26ab50e7dea3f09068e
Marius Markauskas <marius.markauskas1@gmail.com>
2022-05-17 21:45:45 +0300

    Task 1

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
 document.getElementById('submit-btn').addEventListener('click', (event) => {
     event.preventDefault();
 
