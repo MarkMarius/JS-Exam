79ce99e56f5277790f7ae64e86d916809a05eca1
Marius Markauskas <marius.markauskas1@gmail.com>
2022-05-18 19:28:01 +0300

    Tasks

---
+++ Task 3/script.js
@@ -0,0 +1,23 @@
+/* ------------------------------ TASK 3 -----------------------------------
+Parašykite JS kodą, kuris leis vartotojui paspaudus ant mygtuko "Show users"
+pamatyti vartotojus iš Github API (endpoint'as pateiktas žemiau).
+
+Paspaudus mygtuką "Show users":
+1. Informacija atvaizdavima <div id="output"></div> bloke
+1.1. Informacija, kuri pateikiama: "login" ir "avatar_url" reikšmės (kortelėje)
+2. Žinutė "Press "Show Users" button to see users" turi išnykti;
+
+Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi 
+turėti bent minimalų stilių ir būti responsive;
+-------------------------------------------------------------------------- */
+
+const ENDPOINT = 'https://api.github.com/users';
+const results = document.getElementById('output');
+
+const fetchUsers = async (login, avatar_url) => {
+    const response = await fetch(ENDPOINT);
+    const data = await response.json;
+}
+
+
+
