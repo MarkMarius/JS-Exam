42f9236c3d43d841e62272e95b578d8baaed81b5
Marius Markauskas <marius.markauskas1@gmail.com>
2022-05-18 21:12:10 +0300

    Task 3

--- Task 3/script.js
+++ Task 3/script.js
@@ -14,10 +14,34 @@ turėti bent minimalų stilių ir būti responsive;
 const ENDPOINT = 'https://api.github.com/users';
 const results = document.getElementById('output');
 
-const fetchUsers = async (login, avatar_url) => {
-    const response = await fetch(ENDPOINT);
-    const data = await response.json;
+const displayUsers = (user) => {
+    const message = document.getElementById('message');
+    message.innerText = '';
+
+    user.forEach(user => {
+        const login = document.createElement('h3');
+        const img = document.createElement('img');
+
+        img.src = user.avatar_url;
+        login.textContent = user.login;
+
+        results.append(login, img);
+    });
+
 }
+
+const fetchUsers = async () => {
+    const response = await fetch(ENDPOINT);
+    const data = await response.json();
+
+    displayUsers(data);
+}
+
+document.getElementById('btn').addEventListener('click', () => {
+    fetchUsers();
+});
+
+
 
 
 
