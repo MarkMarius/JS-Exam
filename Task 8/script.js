5de49f2f2885de00804a0446027d4895fcd80b87
Marius Markauskas <marius.markauskas1@gmail.com>
2022-05-20 20:27:38 +0300

    Added task 8

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,15 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Constructor(a, b) {
+    this.sum = a + b,
+    this.substraction = a - b,
+    this.multiplication = a * b,
+    this.division = a / b
+}
+
+const calculate = new Constructor(10, 2)
+
+console.log(calculate);
+
