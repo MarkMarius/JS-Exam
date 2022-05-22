022f8d4fc03cfc0b688ed9008c828f75b1f20ab2
Marius Markauskas <marius.markauskas1@gmail.com>
2022-05-22 17:03:44 +0300

    Added Task 9

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,18 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+function Movie(title, director, budget, wasExpensive) {
+    this.title = String;
+    this.director = String;
+    this.budget = Number;
+    this.wasExpensive = function (Number) {
+        if (Number > 100000000) {
+            return true;
+        } else return false;
+    }
+}
+
+const newMovie = new Movie();
+
+console.log(newMovie.wasExpensive(50000000));
